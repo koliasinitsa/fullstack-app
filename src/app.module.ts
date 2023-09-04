a20f6fa9f4f6860c4fbb4c446312baeb25e729f4
koliasinitsa <koliasinitsa317@gmail.com>
2023-09-04 22:44:19 +0300

    connect TypeOrm with Docker compose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({isGlobal: true, envFilePath: '../.env'}),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+				type: 'postgres',
+				host: config.get<string>('TYPEORM_HOST'),
+				username: config.get<string>('TYPEORM_USERNAME'),
+				password: config.get<string>('TYPEORM_PASSWORD'),
+				database: config.get<string>('TYPEORM_DATABASE'),
+				port: config.get<number>('TYPEORM_PORT'),
+				entities: [ __dirname + 'dist/**/*.entity{.ts,.js}' ],
+				synchronize: true,
+				autoLoadEntities: true,
+				logging: true,
+			}),
+    }),
+    UsersModule
+  ],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = await app.get(ConfigService);
+  const port = config.get<number>('API_PORT');
+  await app.listen(port || 3000, () => {
+    console.log(`App started on port ${port}`)
+  });
 }
 bootstrap();
