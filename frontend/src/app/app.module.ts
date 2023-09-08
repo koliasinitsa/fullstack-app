7f2a7cb7b970650e4dfb593f1c1cd4e608f1561b
koliasinitsa <koliasinitsa317@gmail.com>
2023-09-08 21:03:05 +0300

    graphql

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav.component';
 import { IconModule } from './components/icon/icon.module';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { IconModule } from './components/icon/icon.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    IconModule
+    IconModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
